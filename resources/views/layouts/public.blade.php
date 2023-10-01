7b446a3b508190aeb12c3b457a13490bc0e09f20
alessandrostorchi92 <storchi.alle@gmail.com>
2023-10-01 23:16:31 +0200

    Configuration Ok

---
+++ resources/views/layouts/public.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8" />
+    <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="Crea un nuovo progetto Laravel per gestire un archivio di fumetti.">
+
+    <title>Prime operazioni CRUD</title>
+
+    @vite(['resources/scss/app.scss', 'resources/js/app.js'])
+
+</head>
+
+<body>
+
+    <main>
+
+        
+
+    </main>
+
+</body>
+
+</html>
