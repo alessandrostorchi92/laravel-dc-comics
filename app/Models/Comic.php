1567e8d3f44fe5acdbe1b1c1883fc6dc3e477f18
alessandrostorchi92 <storchi.alle@gmail.com>
2023-10-01 23:32:43 +0200

    Creation migration

--- app/Models/Comic.php
+++ app/Models/Comic.php
@@ -5,7 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Comic extends Model
-{
+// Il Model è una rappresentazione sotto forma di classe di una tabella del nostro Database. 
+
+//Tramite questa classe possiamo accedere ed interagire con i dati di quella tabella. 
+
+//TODO Il nome deve essere lo stesso della tabella del DB a cui si riferisce, però al singolare, in PascalCase.
+
+class Comic extends Model {
+
     use HasFactory;
+
 }
