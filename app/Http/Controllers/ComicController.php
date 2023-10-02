70b58445218de2828114c22463bcde486f7985d5
alessandrostorchi92 <storchi.alle@gmail.com>
2023-10-02 16:04:20 +0200

    Creation controller

---
+++ app/Http/Controllers/ComicController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Comic;
+
+//Devo collegare il Model al relativo Controller
+
+class ComicController extends Controller
+{
+    //
+}
--- database/migrations/2023_10_01_213127_create_comics_table.php
+++ database/migrations/2023_10_01_213127_create_comics_table.php
@@ -4,6 +4,12 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+// Ci permettono di manipolare la struttura delle tabelle del database. Possiamo aggiungere, togliere colonne, modificarne gli attributi ed i tipi oppure possiamo completamente eliminare o creare una tabella.
+
+// Tutto quello che faccio nella migration sono tutte operazioni di MySql.
+
+//TODO Il nome deve essere al plurale, minuscolo e in snake_case.
+
 return new class extends Migration
 {
     /**
@@ -14,8 +20,21 @@ return new class extends Migration
     public function up()
     {
         Schema::create('comics', function (Blueprint $table) {
+
             $table->id();
+
+            $table->string("title", 100);
+            $table->text("description");
+            $table->string("thumb");
+            $table->decimal("price", 5, 2);
+            $table->string("series");
+            $table->date("sale_date")->nullable();
+            $table->string("type", 50);
+            $table->json('artists');
+            $table->json('writers');
+
             $table->timestamps();
+
         });
     }
 
