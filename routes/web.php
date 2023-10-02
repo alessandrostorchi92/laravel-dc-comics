0a32e8e2192477da51ed59a2ceb33027cd7fabee
alessandrostorchi92 <storchi.alle@gmail.com>
2023-10-02 18:12:13 +0200

    Creation  Route

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Http\Controllers\ComicController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// 1) Il nome della rotta deve essere il nome dell'entità sulla quale stiamo lavorando
+// 2) Va inserito tral parentesi quadre il NomeController::class e la funzione index()
+// 3) Infine, il nome effettivo della rotta dove verranno visualizzati i dati.
+
+Route::get("/comics", [ComicController::class, "index"])->name("comics.index");
+ 
